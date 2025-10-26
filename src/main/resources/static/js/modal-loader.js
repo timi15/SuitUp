81c79e9e05c0db6be3b86629624eb46c2ab5444e
SzollosBoglarka <szollosboglarka@gmail.com>
2025-10-26 16:55:40 +0100

    feat(wardrobe-items): add edit confirmation

--- src/main/resources/static/js/modal-loader.js
+++ src/main/resources/static/js/modal-loader.js
@@ -25,5 +25,38 @@ document.addEventListener('DOMContentLoaded', () => {
             editModal.querySelector('#edit-material').value = material;
             editModal.querySelector('#edit-imageUrl').value = imageUrl;
         });
+
+        const form = editModal.querySelector('form');
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            Swal.fire({
+                background: "#F7F3E3",
+                color: "#4D1E10",
+                title: 'Biztosan mented a módosításokat?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#6F1A07',
+                cancelButtonColor: '#999999',
+                confirmButtonText: 'Igen, mentés!',
+                cancelButtonText: 'Mégse'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        background: "#F7F3E3",
+                        color: "#4D1E10",
+                        title: 'Mentve!',
+                        icon: 'success',
+                        text: "A módosítások mentése sikeres.",
+                        confirmButtonColor: "#6F1A07",
+                        showConfirmButton: false
+                    })
+                    setTimeout(() => {
+                        form.submit();
+                    }, 1800);
+
+                }
+            });
+        });
     }
 });
